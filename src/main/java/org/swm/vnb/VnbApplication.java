ded9a741e61cd2678a7273fc25ad5799a5b930c6
Esot3riA <esot3ria@gmail.com>
2021-07-23 02:43:46 +0900

    Import jasypt to encrypt db connection info

--- src/main/java/org/swm/vnb/VnbApplication.java
+++ src/main/java/org/swm/vnb/VnbApplication.java
@@ -1,9 +1,11 @@
 package org.swm.vnb;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class VnbApplication {
 
     public static void main(String[] args) {
