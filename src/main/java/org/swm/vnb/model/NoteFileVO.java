1fe606c245b3f22ac59f18f1a1136252fc4e114e
Esot3riA <esot3ria@gmail.com>
2021-08-30 20:47:23 +0900

    Move important column from script to note_file

--- src/main/java/org/swm/vnb/model/NoteFileVO.java
+++ src/main/java/org/swm/vnb/model/NoteFileVO.java
@@ -7,7 +7,9 @@ public class NoteFileVO {
     private Integer file_id;
     private Integer user_id;
     private Integer folder_id;
+    private Integer category_id;
     private String file_name;
+    private Integer is_important;
     private String created_at;
     private String updated_at;
 }
--- src/main/java/org/swm/vnb/model/ScriptVO.java
+++ src/main/java/org/swm/vnb/model/ScriptVO.java
@@ -12,7 +12,6 @@ public class ScriptVO {
     private Integer category_id;
     private String title;
     private Integer is_recording;
-    private Integer is_liked;
     private String audio_file;
     private String video_file;
     private String create_at;
