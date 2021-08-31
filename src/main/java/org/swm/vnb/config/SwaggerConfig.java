030795d583cf1dc3ff1efba30dec9d3e73793f82
Esot3riA <esot3ria@gmail.com>
2021-08-31 10:56:44 +0900

    Add swagger authorization

--- src/main/java/org/swm/vnb/config/SwaggerConfig.java
+++ src/main/java/org/swm/vnb/config/SwaggerConfig.java
@@ -1,14 +1,22 @@
 package org.swm.vnb.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.swm.vnb.jwt.JwtFilter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,8 +25,10 @@ public class SwaggerConfig {
     @Bean
     public Docket restAPI() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.swm.vnb"))
                 .paths(PathSelectors.any())
@@ -32,4 +42,23 @@ public class SwaggerConfig {
                 .description("SW마에스트로 LGTM 팀 비전노트 API 문서")
                 .build();
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", JwtFilter.AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+    }
+
 }
