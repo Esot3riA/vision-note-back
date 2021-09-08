0a050491a54ff38f9a12853d660a991456f1e29c
Esot3riA <esot3ria@gmail.com>
2021-09-08 12:25:43 +0900

    Hide useless properties to VOs

--- src/main/java/org/swm/vnb/model/NoteFolderVO.java
+++ src/main/java/org/swm/vnb/model/NoteFolderVO.java
@@ -1,13 +1,26 @@
 package org.swm.vnb.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class NoteFolderVO {
+
+    @ApiModelProperty(hidden = true)
     private Integer folderId;
+
+    @ApiModelProperty(hidden = true)
     private Integer userId;
+
+    @ApiModelProperty(required = true, example = "1")
     private Integer parentFolderId;
+
+    @ApiModelProperty(required = true)
     private String folderName;
+
+    @ApiModelProperty(hidden = true)
     private String createdAt;
+
+    @ApiModelProperty(hidden = true)
     private String updatedAt;
 }
--- src/main/java/org/swm/vnb/model/ScriptParagraphVO.java
+++ src/main/java/org/swm/vnb/model/ScriptParagraphVO.java
@@ -1,18 +1,41 @@
 package org.swm.vnb.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ScriptParagraphVO {
+
+    @ApiModelProperty(hidden = true)
     private Integer paragraphId;
+
+    @ApiModelProperty(hidden = true)
     private Integer scriptId;
+
+    @ApiModelProperty(hidden = true)
     private Integer userId;
+
+    @ApiModelProperty(required = true, example = "1")
     private Integer paragraphSequence;
+
+    @ApiModelProperty(required = true, example = "00:00")
     private String startTime;
+
+    @ApiModelProperty(required = true, example = "01:00")
     private String endTime;
+
+    @ApiModelProperty(required = true)
     private String paragraphContent;
+
+    @ApiModelProperty(required = true)
     private String memoContent;
+
+    @ApiModelProperty(required = true, example = "0")
     private Integer isBookmarked;
+
+    @ApiModelProperty(hidden = true)
     private String createdAt;
+
+    @ApiModelProperty(hidden = true)
     private String updatedAt;
 }
