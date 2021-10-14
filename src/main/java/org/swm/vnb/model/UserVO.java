9178828e73117b13ba96e539cd747d39c0a604b0
Esot3riA <esot3ria@gmail.com>
2021-10-14 15:37:04 +0900

    Fix user modify error

--- src/main/java/org/swm/vnb/model/UserVO.java
+++ src/main/java/org/swm/vnb/model/UserVO.java
@@ -1,8 +1,10 @@
 package org.swm.vnb.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVO implements UserDetails {
 
     @ApiModelProperty(hidden = true)
